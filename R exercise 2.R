209e36ab8ae2cd03bea57cf5da2ef4be95f208b8
kelseygonzalez <50840312+kelseygonzalez@users.noreply.github.com>
2019-09-15 19:30:07 -0700

    Delete R exercise 2.R

--- R exercise 2.R
+++
@@ -1,9 +0,0 @@
-########################################################################################################
-# Creators: Kelsey Gonzalez and Laura Werthmann 
-# Title: Group 9: R exercise 2: Data Preprocessing
-# Date created: 9/15/2019
-# Date last modified:
-# Due Date: 10/02/2019
-########################################################################################################
-
-load("LifeExpectancyData.Rdata")
