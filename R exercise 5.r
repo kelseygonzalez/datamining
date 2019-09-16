fff2391f2be01984394bcd8041bfeb0dec64fa17
kelseygonzalez <kelseygonzalez@email.arizona.edu>
2019-09-15 17:03:52 -0700

    Create do-files for the semester

---
+++ R exercise 5.r
@@ -0,0 +1,9 @@
+########################################################################################################
+# Creators: Kelsey Gonzalez and Laura Werthmann 
+# Title: Group 9: R exercise 5: Classification
+# Date created: 9/15/2019
+# Date last modified:
+# Due Date: 11/06/2019
+########################################################################################################
+
+load("LifeExpectancyData.Rdata")
---
+++ R exercise 6.r
@@ -0,0 +1,9 @@
+########################################################################################################
+# Creators: Kelsey Gonzalez and Laura Werthmann 
+# Title: Group 9: R exercise 6: Clustering
+# Date created: 9/15/2019
+# Date last modified:
+# Due Date: 11/27/2019
+########################################################################################################
+
+load("LifeExpectancyData.Rdata")
