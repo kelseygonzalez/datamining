6acd80ff2ec842537ac51893c2facfbcdfd0066c
kelseygonzalez <50840312+kelseygonzalez@users.noreply.github.com>
2019-09-15 19:30:47 -0700

    Delete save as rdata.r

--- save as rdata.r
+++
@@ -1,4 +0,0 @@
-library(readr)
-setwd("C:/Users/ckgon/Google Drive/2019/3 Fall 2019/Data Mining/Datasets/")
-data <-read_csv("LifeExpectancyData.csv", col_names = TRUE, na="")
-save(data, file="LifeExpectancyData.Rdata")
