aa94f7f35943a97df4a6396d21a6952aaeb80f4b
kelseygonzalez <50840312+kelseygonzalez@users.noreply.github.com>
2019-09-15 19:30:15 -0700

    Delete R exercise 3.R

--- R exercise 3.R
+++
@@ -1,9 +0,0 @@
-########################################################################################################
-# Creators: Kelsey Gonzalez and Laura Werthmann 
-# Title: Group 9: R exercise 3: OLAP Operations in R
-# Date created: 9/15/2019
-# Date last modified:
-# Due Date: 10/09/2019
-########################################################################################################
-
-load("LifeExpectancyData.Rdata")
