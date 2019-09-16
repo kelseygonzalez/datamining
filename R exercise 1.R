826ea46471682c3be9eef85aabdca6b6205d35fc
kelseygonzalez <kelseygonzalez@email.arizona.edu>
2019-09-15 17:03:52 -0700

    Create do-files for the semester

---
+++ R exercise 1.R
@@ -0,0 +1,9 @@
+########################################################################################################
+# Creators: Kelsey Gonzalez and Laura Werthmann 
+# Title: Group 9: R exercise 1: Know Your Data
+# Date created: 9/15/2019
+# Date last modified:
+# Due Date: 9/18/2019
+########################################################################################################
+ 
+load("LifeExpectancyData.Rdata")
---
+++ R exercise 4.r
@@ -0,0 +1,9 @@
+########################################################################################################
+# Creators: Kelsey Gonzalez and Laura Werthmann 
+# Title: Group 9: R exercise 4: Association Rule Mining
+# Date created: 9/15/2019
+# Date last modified:
+# Due Date: 10/23/2019
+########################################################################################################
+
+load("LifeExpectancyData.Rdata")
